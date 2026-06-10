d02100a94c866665703c3ca87b2b797a4b68fed4
tranvanhuy <tranvanhuy064206@gmail.com>
2026-06-10 18:57:27 +0700

    Update

--- src/views/auth/Register.js
+++ src/views/auth/Register.js
@@ -1,29 +1,29 @@
-import { ref } from 'vue';
-import { useAuthStore } from '../../stores/auth.js';
-import { useRouter } from 'vue-router';
+import { ref } from "vue";
+import { useAuthStore } from "../../stores/auth.js";
+import { useRouter } from "vue-router";
 
 export default {
-  name: 'Register',
+  name: "Register",
   setup() {
     const authStore = useAuthStore();
     const router = useRouter();
 
-    const username = ref('');
-    const password = ref('');
-    const fullName = ref('');
-    const phone = ref('');
-    const email = ref('');
-    const identityCard = ref('');
-    const idCardIssueDate = ref('');
-    const idCardIssuePlace = ref('');
-    
-    const error = ref('');
-    const success = ref('');
+    const username = ref("");
+    const password = ref("");
+    const fullName = ref("");
+    const phone = ref("");
+    const email = ref("");
+    const identityCard = ref("");
+    const idCardIssueDate = ref("");
+    const idCardIssuePlace = ref("");
+
+    const error = ref("");
+    const success = ref("");
     const loading = ref(false);
 
     const handleRegister = async () => {
-      error.value = '';
-      success.value = '';
+      error.value = "";
+      success.value = "";
       loading.value = true;
       try {
         await authStore.register(
@@ -32,17 +32,18 @@ export default {
           email.value,
           phone.value,
           fullName.value,
-          'LANDLORD', // Vai trò mặc định khi tự đăng ký là CHỦ TRỌ
+          "LANDLORD", // Vai trò mặc định khi tự đăng ký là CHỦ TRỌ
           identityCard.value,
           idCardIssueDate.value ? idCardIssueDate.value : null,
-          idCardIssuePlace.value
+          idCardIssuePlace.value,
         );
 
-        success.value = 'Đăng ký tài khoản thành công! Đang chuyển về trang Đăng nhập...';
-        
+        success.value =
+          "Đăng ký tài khoản thành công! Đang chuyển về trang Đăng nhập...";
+
         // Trì hoãn 2 giây rồi chuyển trang
         setTimeout(() => {
-          router.push('/login');
+          router.push("/login");
         }, 2000);
       } catch (err) {
         error.value = err;
